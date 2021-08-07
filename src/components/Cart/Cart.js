5d647452a2629e8ee0d72b025dde688766fedbad
Takumikimura141899 <kimura141899@gmail.com>
2021-08-08 08:34:08 +0900

    create CartItem

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,30 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `￥${cartCtx.totalAmount.toLocaleString()}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 1, name: 'sushi', amount: 2, price: 1980 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -14,13 +33,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>合計金額</span>
-        <span>￥3000</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           閉じる
         </button>
-        <button className={classes.button}>注文する</button>
+        {hasItems && <button className={classes.button}>注文する</button>}
       </div>
     </Modal>
   );

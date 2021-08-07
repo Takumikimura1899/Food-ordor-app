769155daa91142c2a148350e9e16c9c270458f91
Takumikimura141899 <kimura141899@gmail.com>
2021-08-07 14:18:33 +0900

    create CartIcon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import mealsImage from '../../assets/meals.jpg';
 
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Reactミール</h1>
-        <button>カート</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='テーブルいっぱいの美味しそうな料理たち' />
